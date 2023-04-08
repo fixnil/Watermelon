ea59b95483511e683344b0063bad6c141c8fd184
fixnil <deping.du@gmail.com>
2023-04-09 00:32:09 +0800

    feat: 完成分数控制器

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,18 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static ScoreManager Instance;
+
+    private int _score;
+    private Text _scoreText;
+
+    public int Score
     {
-        
+        get => _score;
+        set
+        {
+            _score = value;
+            _scoreText.text = value.ToString();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        _scoreText = this.transform.Find("ScoreText").GetComponent<Text>();
     }
 }
