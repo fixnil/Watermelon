4cf2bd1839c95e97b99c6d11998b98769d47dab5
fixnil <deping.du@gmail.com>
2023-04-09 00:32:32 +0800

    feat: 完成音效播放器

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,18 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static AudioManager Instance;
+
+    public AudioClip MergeClip;
+    public AudioClip CollideClip;
+
+    private  AudioSource _audioSource;
+
+    private void Awake()
     {
-        
+        Instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        _audioSource = this.GetComponent<AudioSource>();
+    }
+
+    public void PlayMergeClip()
+    {
+        _audioSource.PlayOneShot(MergeClip);
+    }
+
+    public void PlayCollideClip()
+    {
+        _audioSource.PlayOneShot(CollideClip);
     }
 }
