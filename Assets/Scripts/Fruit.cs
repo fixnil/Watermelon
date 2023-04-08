3d9aa3bd769b25b6f3a72167219e480689fc86a3
fixnil <deping.du@gmail.com>
2023-04-09 00:31:46 +0800

    feat: 初始化水果 Level

--- Assets/Scripts/Fruit.cs
+++ Assets/Scripts/Fruit.cs
@@ -1,18 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Fruit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int Level;
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+
     }
 }
