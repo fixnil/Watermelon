6732fc53f1d54a7dabc0e8512d3c4c72148f3690
fixnil <deping.du@gmail.com>
2023-04-09 00:53:59 +0800

    feat: 完成水果下落

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,11 +15,57 @@ public class Player : MonoBehaviour
 
     private void Start()
     {
-
+        this.CreateFruit();
     }
 
     private void Update()
     {
+        if (ReadyFruit == null)
+        {
+            return;
+        }
 
+        if (Input.GetMouseButton(0))
+        {
+            var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            mousePosition.y = ReadyFruit.transform.position.y;
+            mousePosition.z = ReadyFruit.transform.position.z;
+
+            var radius = ReadyFruit.GetComponent<CircleCollider2D>().radius;
+            var minX = -3.2 + radius;
+            var maxX = 3.2 - radius;
+
+            if (mousePosition.x < minX)
+            {
+                mousePosition.x = (float)minX;
+            }
+            else if (mousePosition.x > maxX)
+            {
+                mousePosition.x = (float)maxX;
+            }
+
+            ReadyFruit.transform.position = mousePosition;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            ReadyFruit.GetComponent<Rigidbody2D>().gravityScale = 1;
+
+            ReadyFruit = null;
+
+            this.Invoke(nameof(this.CreateFruit), 0.8F);
+        }
+    }
+
+    private void CreateFruit()
+    {
+        var index = Random.Range(0, 4);
+
+        ReadyFruit = Instantiate(FruitPrefabs[index]);
+
+        ReadyFruit.transform.position = InitLocation.position;
+
+        ReadyFruit.GetComponent<Rigidbody2D>().gravityScale = 0;
     }
 }
